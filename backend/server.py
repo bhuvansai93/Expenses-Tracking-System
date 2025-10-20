8018b07224a039ebe331df427bf0857ad345bf02
bhuvansai93 <bhuvansai93@gmail.com>
2025-10-20 23:33:36 +0100

    update

--- backend/server.py
+++ backend/server.py
@@ -3,6 +3,9 @@ from datetime import date
 import db_helper
 from typing import List
 from pydantic import BaseModel
+import sys, os
+sys.path.append(os.path.dirname(__file__))
+
 
 app = FastAPI()
 
